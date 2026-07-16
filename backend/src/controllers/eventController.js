c2a05b5a9bae50bee9efd5893c5e2d607004c737
phanithatigiri <phanithatigiri123@gmail.com>
2026-07-16 14:55:02 +0530

    added new forgot password link

--- backend/src/controllers/eventController.js
+++ backend/src/controllers/eventController.js
@@ -16,7 +16,7 @@ export const createEvent = async (req, res) => {
       seats: rawSeats,
     } = req.body;
 
-    // ---------------- VALIDATION ----------------
+    // ---------------- VALIDATION  ----------------
     if (
       !title ||
       !description ||
